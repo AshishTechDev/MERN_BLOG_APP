8017d5ecb9edc10d5bc0749a317b95ec82d23709
superashishb <bhandariashish2022@gmail.com>
2024-02-15 12:05:10 +0530

    create the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Header from './components/Header.jsx'
 import Home from './pages/Home.jsx'
 import SignUp from './pages/SignUp.jsx'
+import SignIn from './pages/SignIn.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import Projects from './pages/Projects.jsx'
+import About from './pages/About.jsx'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/signup' element={<SignUp/>} />
--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server : {
-    Port : 1000,
+    Port : 3000,
   },
 })
