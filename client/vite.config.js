beb3ed2514cee85662441dd20e3fe02a35d74082
superashishb <bhandariashish2022@gmail.com>
2024-03-04 15:05:57 +0530

    add functionallity like use navigate

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   plugins: [react()],
   server : {
-    Port : 3000,
+    port : 2000,
+    proxy : {
+      '/api' : {
+        target : 'http://localhost:3000',
+        secure: false,
+      },
+    },
   },
 })
