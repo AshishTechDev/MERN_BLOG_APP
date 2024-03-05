7c974ab69773b6514d378dfa65115de9ed0af281
superashishb <bhandariashish2022@gmail.com>
2024-03-05 19:30:42 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Link, useNavigate } from 'react-router-dom' ;
 import {Alert, Button, Label, Spinner, TextInput} from 'flowbite-react' ;
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice' ;
 
 export default function SignIn() {
     const [formData, setFormData] = useState({}) ;
-    const [errorMessage, setErrorMessage] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const {loading, error: errorMessage} = useSelector(state => state.user);
+    const dispatch = useDispatch();
     const navigate = useNavigate();
   const handleChange =(e) => {
         setFormData({...formData, [e.target.id] : e.target.value.trim() });
@@ -16,11 +18,10 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
         e.preventDefault();
         if( !formData.email || !formData.password){
-          return setErrorMessage('Please fill out all fields.');
+          return dispatch(signInFailure('Please fill out all fields.'));
         }
         try{
-          setLoading(true);
-          setErrorMessage(null);
+          dispatch(signInStart());
           const res = await fetch('/api/auth/signin',{
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -28,20 +29,16 @@ export default function SignIn() {
           });
           const data = await res.json();
           if(data.success === false) {
-            return setErrorMessage(data.message);
+            dispatch(signInFailure(data.message));
           }
-          setLoading(false);
           if(res.ok){
+            dispatch(signInSuccess(data))
             navigate('/');
-
-            // 2:20:46 
           }
-        } catch(err){
-          setErrorMessage(err.message);
-          setLoading(false);
+        } catch(error){
+          dispatch(signInFailure(error.message));
         }
   }
-  console.log(formData);
   return (
     <div className='min-h-screen mt-20'>
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -54,7 +51,7 @@ export default function SignIn() {
         </span>BLOG
       </Link>
       <p className='text-sm mt-5'>
-        This is a demo project. You can signi in with your email and password or with Google.
+        This is a demo project. You can signin in with your email and password or with Google.
       </p>
       </div>
 
