d4114579e918ce1b033788145c5c6c85124c4e27
superashishb <bhandariashish2022@gmail.com>
2024-06-08 11:11:55 +0530

    create get users api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -7,7 +7,9 @@ import { verifyToken } from '../utils/verifyUser.js';
   router.get('/test', test);
   router.put('/update/:userId', verifyToken , updateUser);
   router.delete('/delete/:userId', verifyToken, deleteUser);
-  router.post('/signout', signout)
+  router.post('/signout', signout);
+  
+  router.get('/getusers', verifyToken, getUsers);
 
   export default router;
   
