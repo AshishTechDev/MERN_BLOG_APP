87fb3bc3bb1753561e0b4101a2ce0a29dc07a0ab
superashishb <bhandariashish2022@gmail.com>
2024-06-20 21:56:28 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,13 +6,18 @@ import authRoutes from './routes/auth.route.js' ;
 import postRoutes from './routes/post.route.js' ;
 import commentRoutes from './routes/comment.route.js' ;
 import cookieParser from 'cookie-parser';
+import path from 'path' ;
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
   console.log('Database connection established');
 }).catch((err) => {
   console.log(err) ;
-})
+});
+
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -26,6 +31,12 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client' , 'dist', 'index.html'));
+});
 
 
 app.use((err, req, res, next) => {
