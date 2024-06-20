e1bdfb1c4d97dc396854d9218e0f9dc5a5481c01
superashishb <bhandariashish2022@gmail.com>
2024-06-20 21:47:46 +0530

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-top justify-center'>
+      <div className='max-w-4xl mx-auto p-3 text-center'>
+          <div className='mt-24'>
+            <h1 className='text-5xl font font-semibold text-center my-12'>About Ashish Blog</h1>
+            <div className='text-lg text-gray-500 flex flex-col gap-6'>
+                <p>
+                  Welcome to Ashish's Blog This blog was created by Ashish Bhandari as a personal project to share his thoughts and ideas with the world. Ashish is a passionate developer who loves to write about technology, coding and everything in between.
+                </p>
+                <p>
+                  On this blog, you'll find weekly articles and tutorials on topics such as web development, software development, and programming languages, Ashish is always learing and exploring new technologies, so be sure to check back often for new content!
+                </p>
+              <p>
+                We encourage you to leave comments on our posts and engage with other readers. You can like other people's comments and reply to them as well. We believe that a community of learners can help each other grow and improve.
+              </p>
+            </div>
+          </div>
+      </div>
+    </div>
   )
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import CallToAction from '../components/CallToAction.jsx' ;
 
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div className='min-h-screen max-w-3xl mx-auto flex justify-top items-center flex-col gap-6 p-3'>
+      <h1 className='text-5xl font-semibold mt-24'>Projects</h1>
+      <p className='text-md text-gray-500 mt-0 my-12'>Build fun and engaging projects while learning HTML,CSS and Javascript!</p>
+      <CallToAction />
+    </div>
   )
 }
